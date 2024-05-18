a98e6053b45927b86f1e43c9e4cfe2bfef276dad
Lucas Cubile <cubilelucas@gmail.com>
2024-05-18 18:09:18 -0300

    remove unnecesary iteration from loop

--- Desafío_2_Equipo_40_Code_Mavs.py
+++ Desafío_2_Equipo_40_Code_Mavs.py
@@ -10,6 +10,8 @@ def contar_pases_y_efectividad():
 
   Por ultimo la función toma el diccionario de equipos y por cada par clave: valor, crea un diccionario individual los cuales seran devueltos dentro de una lista.
 
+  La función arrojara un error por consola en caso de haber un problema en la apertura del archivo de datos.
+
   Valor de retorno:
     [
       {team_1: [{},{},...]},
@@ -18,14 +20,18 @@ def contar_pases_y_efectividad():
   Tipo de dato de retorno:
     Lista de diccionarios de equipos cuyo valor es una lista de diccionarios de jugadores.
   '''
-
-  with open("pases.txt", 'r') as file:
-    for i in file:
-      team, number, name, pass_status, _ = i.split(';')
-      save_player(name, team, number, pass_status)
-  
-  sort_players_by_pass_efficiency()
-  return [{key: value} for key, value in teams.items()]
+  try:
+    with open("pases.txt", 'r') as file:
+      for i in file:
+        team, number, name, pass_status, _ = i.split(';')
+        save_player(name, team, number, pass_status)
+    
+    sort_players_by_pass_efficiency()
+    print("Registro de pases creado con éxito")
+    return [{key: value} for key, value in teams.items()]
+  except Exception as err:
+    print(f"Hubo un problema: {err}")
+    
 
 def find_player(name, team, number):
 
@@ -68,6 +74,8 @@ def save_player(name, team, number, pass_status):
 
   Función para guardar los jugadores en sus respectivos equipos y actualizar sus datos y estadísticas de pases.
   Lo primero que hace es consultar con un if si el equipo ya se encuentra agregado o si hay que agregarlo (se inicializa el equipo con un lista vacio de jugadores. Se resolvio de esta manera para contemplar casos de mas de dos equipos), luego con la función find_player busca al jugador correspondiente y al final recorre el lista de jugadores que tiene el equipo con una busqueda lineal para poder encontrar al jugador que se requiere y actualiza los pases totales, el porcentaje de pases acertados y sus pases exitosos/fallidos dependiendo del status del mismo pasado por parametro.
+
+  El bucle dejara de iterar una vez que el jugador sea encontrado y sus datos hayan sido actualizados.
   '''
 
   if team not in teams:
@@ -82,6 +90,7 @@ def save_player(name, team, number, pass_status):
       else:
         player["pases_mal"] += 1
       player["porcentaje"] = round((player["pases_bien"] * 100) / player["cantidad_pases"], 2)
+      break
 
 def sort_players_by_pass_efficiency():
 
